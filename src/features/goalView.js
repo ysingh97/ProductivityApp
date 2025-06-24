af61a0edffb249aafcc8206eef389919c28f8681
ysingh97 <bubstubs@gmail.com>
2025-06-23 23:52:08 -0700

    displaying parent goal in goal view

--- src/features/goalView.js
+++ src/features/goalView.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchGoalById } from '../services/goalService';
 import { Link } from "react-router-dom";
 
 const GoalView = ({ goal }) => {
-
+    const [parentGoal, setParentGoal] = useState(null);
     console.log("rendering goalview goal: ", goal);
+
+    useEffect(() => {
+        console.log("goal view useeffect: goal", goal);
+        if (goal && goal.parentGoal) {
+            const loadData = async() => {
+                try {
+                    const parentGoal = await fetchGoalById(goal.parentGoal);
+                    setParentGoal(parentGoal);
+                } catch (err) {
+                    console.error(err.message);
+                }
+            }
+            loadData();
+        }
+    }, [goal]);
 
     if (!goal) {
         return <p>Loading goal...</p>;
@@ -14,6 +30,7 @@ const GoalView = ({ goal }) => {
             <p>Goal test</p>
             <p>Goal: {goal.title}</p>
             <p>Description: {goal.description}</p>
+            {parentGoal && <p>Parent Goal: {parentGoal.title}</p>}
             <Link
                 to="/createGoalPage"
                 state={{ parentGoal: goal, isParentGoalFixed: true }}
